ccb66d783d8ca7d6b6ae5c9af245821bb2987451
jason <liaowuhen821@126.com>
2017-12-25 09:08:21 +0800

    kafka produce string

---
+++ src/main/java/kafka/ProducerString.java
@@ -0,0 +1,54 @@
+package kafka;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import utils.AvroUtils;
+import utils.ByteUtil;
+import zookeeper.ZookeeperTemplate;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by jason on 17-9-14.
+ */
+public class ProducerString {
+    public static void main(String[] args) throws IOException {
+        String bootstrapServers = "11.11.60.99:6667,11.11.60.100:6667,11.11.60.101:6667";
+
+        InputStream inputStream = ProducerString.class.getClassLoader().getResourceAsStream("dns.json");
+        String dnsStr = IOUtils.toString(inputStream);
+        System.out.println(dnsStr);
+        JSONObject dnsJsonObject = JSONObject.parseObject(dnsStr);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("test-zxl", null, dnsJsonObject.toJSONString());
+
+        Properties kafkaProps = new Properties();
+        kafkaProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        //Defaults overridden based on config
+        kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
+        int i = 0;
+        while (i < 100) {
+            producer.send(record);
+            i++;
+
+        }
+        producer.flush();
+
+
+    }
+}
