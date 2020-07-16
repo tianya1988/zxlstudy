d666b0f525149feaa4a4e2be63734bbdbdafbae2
zhangxuelong <zhangxuelong@scsc.tech>
2020-07-16 11:17:16 +0800

    update read xlsx file

--- src/main/java/excel/ReadExcelDemoXLSX.java
+++ src/main/java/excel/ReadExcelDemoXLSX.java
@@ -2,125 +2,65 @@
 package excel;
 
 
-import org.apache.poi.ss.usermodel.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class ReadExcelDemoXLSX {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         readExcel("/home/jason/Desktop/APPbushu/jiaotong/screen/ftp-data/osscan_excel_info2.xlsx");
     }
 
-    public static void readExcel(String path) {
+    public static void readExcel(String path) throws IOException {
         File file = new File(path);
         FileInputStream fileInputStream = null;
         Workbook workBook = null;
 
         if (file.exists()) {
-            try {
-                fileInputStream = new FileInputStream(file);
-//                workBook = WorkbookFactory.create(fileInputStream);
-                XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
-                XSSFSheet xssfSheet = xssfWorkbook.getSheet("主机IP");
+            fileInputStream = new FileInputStream(file);
+            // workBook = WorkbookFactory.create(fileInputStream);
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+            XSSFSheet xssfSheet = xssfWorkbook.getSheet("漏洞详情");
 
 
-                int numberOfSheets = xssfWorkbook.getNumberOfSheets();
-                System.out.println("number of sheets is : " + numberOfSheets);
+            int numberOfSheets = xssfWorkbook.getNumberOfSheets();
+            System.out.println("number of sheets is : " + numberOfSheets);
+            System.out.println("number of rows is : " + xssfSheet.getPhysicalNumberOfRows());
 
-                // sheet0工作表
-                for (int s = 0; s < 1; s++) {
-                    Sheet sheetAt = xssfWorkbook.getSheetAt(s);
-                    //获取工作表名称
-                    String sheetName = sheetAt.getSheetName();
-                    System.out.println("工作表名称：" + sheetName);
-                    // 获取当前Sheet的总行数
-                    int rowsOfSheet = sheetAt.getPhysicalNumberOfRows();
-                    System.out.println("当前表格的总行数:" + rowsOfSheet);
+            // 此list是为了能够记录合并的单元格,设计的数据结构; 表格中总行数为拆分后的单元格(与表行的行号对应).
+            List<HashMap<Integer, String>> list = new LinkedList<HashMap<Integer, String>>();
+            int physicalNumberOfRows = xssfSheet.getPhysicalNumberOfRows();
+            for (int rowNum = 0; rowNum < physicalNumberOfRows; rowNum++) {
+                XSSFRow row = xssfSheet.getRow(rowNum);
+                int numberOfCells = row.getPhysicalNumberOfCells();
 
-                    int beginRowNum = 0;
-
-                    //跳过第一行(此为合并行);
-                    for (int i = 1; i < rowsOfSheet; i++) {
-                        Row row = sheetAt.getRow(i);
-                        if ("编号".equals(row.getCell(0).getStringCellValue()) && "时间".equals(row.getCell(1).getStringCellValue())) {
-                            beginRowNum = i;
-                        }
-                    }
-                    System.out.println("beginRowNum is : " + beginRowNum);
-
-                    // 计算 beginRow 一共有几列
-                    Row beginRow = sheetAt.getRow(beginRowNum);
-                    int physicalNumberOfCells = sheetAt.getRow(beginRowNum).getPhysicalNumberOfCells();
-                    System.out.println("physicalNumberOfCells is : " + physicalNumberOfCells);
-
-                    // 统计列名  :  编号  时间  温度°C
-                    String[] title = new String[physicalNumberOfCells];
-                    for (int i = 0; i < physicalNumberOfCells; i++) {
-                        title[i] = beginRow.getCell(i).getStringCellValue();
-                    }
-
-                    for (int r = (beginRowNum + 1); r < rowsOfSheet; r++) {
-                        Row row = sheetAt.getRow(r);
-                        if (row == null) {
-                            continue;
-                        } else {
-                            int rowNum = row.getRowNum() + 1;
-                            System.out.println("当前行:" + rowNum);
-
-                            for (int columnNum = 0; columnNum < physicalNumberOfCells; columnNum++) {
-                                Cell cell = row.getCell(columnNum);
-                                if ((cell.getCellTypeEnum() == CellType.STRING)) {
-                                    String cellValue = cell.getStringCellValue();
-                                    System.out.println(title[columnNum] + " : " + cellValue);
-                                } else {
-                                    System.out.println("第" + rowNum + "行，第" + (columnNum + 1) + "列[" + title[columnNum] + "]数据错误！");
-                                }
-                            }
-
-                            /*
-                            // 总列(格)
-                            Cell cell0 = row.getCell(0);
-                            Cell cell1 = row.getCell(1);
-                            Cell cell2 = row.getCell(2);
-
-
-                            if ((cell0.getCellTypeEnum() == CellType.STRING)) {
-                                String numericCellValue = cell0.getStringCellValue();
-                                System.out.println(numericCellValue);
-                            } else {
-                                System.out.println("第" + rowNum + "行，第一列[" + title[0] + "]数据错误！");
-                            }
-                            if (cell1.getCellTypeEnum() == CellType.STRING) {
-                                String stringCellValue = cell1.getStringCellValue();
-                                System.out.println(stringCellValue);
-                            } else {
-                                System.out.println("第" + rowNum + "行，第二列[" + title[1] + "]数据错误！");
-                            }
-                            if (cell2.getCellTypeEnum() == CellType.STRING) {
-                                String stringCellValue = cell2.getStringCellValue();
-                                System.out.println(stringCellValue);
-                            } else {
-                                System.out.println("第" + rowNum + "行，第三列[" + title[2] + "]数据错误！");
-                            }
-                            */
-
-                            System.out.println("=============");
-                        }
+                HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+                for (int column = 0; column < numberOfCells; column++) {
+                    if (StringUtils.isEmpty(row.getCell(column).getStringCellValue())) {
+                        /**
+                         * 合并后的单元格,在实际读取的时候,相当于是首行的值,其他行值为空
+                         * 当遇到空的时候,应该从上一行记录中读取值
+                         */
+                        hashMap.put(column, list.get(rowNum -1).get(column));
+                    } else {
+                        hashMap.put(column, row.getCell(column).getStringCellValue());
                     }
                 }
-                if (fileInputStream != null) {
-                    fileInputStream.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                list.add(hashMap);
             }
-        } else {
-            System.out.println("文件不存在!");
+            System.out.println(list.size());
         }
     }
 }
