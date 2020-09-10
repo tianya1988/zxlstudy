fa6080d69b99b284509342c82f65e0cc6be87915
zhangxuelong <zhangxuelong@scsc.tech>
2020-09-10 17:12:50 +0800

    rsa guidaojiaotong

--- src/main/java/rsa/RSAEncrypt2.java
+++ src/main/java/rsa/RSAEncrypt2.java
@@ -32,20 +32,21 @@ public class RSAEncrypt2 {
         genKeyPair();
 
         String domainNameMD5 = MD5Util.getMD5("xingtu.scsc.tech");
-        System.out.println("domainNameMD5 is : " + domainNameMD5);
+        System.out.println("domainNameMD5 is : " + domainNameMD5);//验证代码中，这个串被写死进去了。所以message中xingtu.scsc.tech这个字符串不可以变
 
         //测试匹配的秘钥对的情况
         //keyMap.put(0, "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvXGi0H5uqtj0kcPOmfDinsChai/Wjyb0EegE+2phFJoY8YU+ayOc1bdQqBhgXQSOL13Sbk58lPwuANcvb1UfBL12/7TJHWOAxq3eOgdsotKBn4iN/cT9kd+MzmsKZgR6GuFyzNcz0RQixWxQG+B1WjiDZJZvhe54LrhfwjNIB9wIDAQAB");
         //keyMap.put(1, "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAK9caLQfm6q2PSRw86Z8OKewKFqL9aPJvQR6AT7amEUmhjxhT5rI5zVt1CoGGBdBI4vXdJuTnyU/C4A1y9vVR8EvXb/tMkdY4DGrd46B2yi0oGfiI39xP2R34zOawpmBHoa4XLM1zPRFCLFbFAb4HVaOINklm+F7nguuF/CM0gH3AgMBAAECgYB0y0ZNGXQzWdcVK0mMC9YsEU5/KBJ9eQK0NZIYy7x46fSjyGgdfktHsZOKPa7T5iWHoQNgDw0C4J+HT80r6Dw2GeRYn7B8rxvPa+7JhyPZmP8xn+AGdKDHjY+9HsZZSp7yGDX2GnHiQ9/bz62OjX0zSAjqVNE6kEavhI98gfvcEQJBAOqT6yOspqJV/ATe3YNe9f41z+r1Vgp2qXzq8U71CXxEAOGZoqVyYOrz9WmsqLeHfwrstUiYYqh1rCrrV+6RrK8CQQC/YBayj1sHIiTy54ILoddL0+I9NcjENi/ugseXk1u1oNMt6MHLg4nJm8biRR8LlcXXCU/bp9Lfa341TJPS4CE5AkEA1jyNziEKdsxsxM3+JNL/e1IOOBokoJ3LIUk5ZdEo/pLk8yVrhAq4NRzlj8Oeuy9GeeXBkzGJOS1vNiuGPV5SBQJBAK1hsc6eQFsrHQyQn+pH+xVw5oSMKNNTQIO3Knrx/2GDTjBGsuTdzETChCJVezX0wGP8xMP9vPe7nIFPWpHvi0kCQAEoFt8KAQk3mb9FpBA+FLch/wokA7qNdn8TWMkHGR8kXV9zIH4PdIUG4eingUdSvBCu9P9kEeFNVEum5WCZzLU=");
 
-        String endDateStr = "2020-11-10 16:17:00";
+
+        // 轨道交通
+        String endDateStr = "2020-10-10 17:00:00";//参与验证
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         long endTime = sdf.parse(endDateStr).getTime();
 
-        // 轨道交通
         String ip = "172.16.0.204";
-        String mac = "20:04:0F:E6:32:34"; //注意要大写
-        String osVersion = "3.10.0-514.el7.x86_64";
+        String mac = "20:04:0F:E6:32:34"; //注意要大写,参与验证
+        String osVersion = "3.10.0-514.el7.x86_64";//参与验证
 
         //加密字符串
         String message = ip + "," + mac + ",www.scsc.tech,xingtu.scsc.tech," + endTime + "," + osVersion;
